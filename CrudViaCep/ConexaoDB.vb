e4f5b696853e709b090d537593b575fae3045610
Felipe Eduardo <felipe.e.w@hotmail.com>
2019-07-22 21:01:18 -0300

    Estruturas do Banco de Dados - ISSUE #1 concluida

--- CrudViaCep/ConexaoDB.vb
+++ CrudViaCep/ConexaoDB.vb
@@ -2,7 +2,7 @@
 
 Public Class ConexaoDB
 
-    Public Const strConexao As String = "Data Source=;Initial Catalog=Cep;Integrated Security=True"
+    Public Const strConexao As String = "Data Source=ANDRE\SQLEXPRESS;Initial Catalog=CrudViaCep;Integrated Security=True"
     Public strInstrucao As String = String.Empty
     Public objConexao As New SqlConnection(strConexao)
     Public objCommand As New SqlCommand(strInstrucao, objConexao)
@@ -30,24 +30,51 @@ Public Class ConexaoDB
 
     Public Sub EditarCep(ByVal cep As Cep)
 
-        strInstrucao = "UPDATE Cep SET Cep=@Cep, Logradouro=@Logradouro, Complemento=@Complemento, Bairro=@Bairro, Localidade=@Localidade, Estado=@Estado, Unidade=@Unidade, Ibge=@Ibge, Gia=@Gia WHERE CepId=@CepId"
+        strInstrucao = "UPDATE Cep SET Cep = @Cep, Logradouro = @Logradouro, Complemento = @Complemento, Bairro = @Bairro, Localidade = @Localidade, Estado = @Estado, Unidade = @Unidade, Ibge = @Ibge, Gia = @Gia WHERE CepId = @CepId"
+        objCommand.CommandText = strInstrucao
+        objCommand.Connection = objConexao
+        objCommand.Parameters.AddWithValue("@Cep", cep.Cep)
+        objCommand.Parameters.AddWithValue("@Logradouro", cep.Logradouro)
+        objCommand.Parameters.AddWithValue("@Complemento", cep.Complemento)
+        objCommand.Parameters.AddWithValue("@Bairro", cep.Bairro)
+        objCommand.Parameters.AddWithValue("@Localidade", cep.Localidade)
+        objCommand.Parameters.AddWithValue("@Estado", cep.Estado)
+        objCommand.Parameters.AddWithValue("@Unidade", cep.Unidade)
+        objCommand.Parameters.AddWithValue("@Ibge", cep.Ibge)
+        objCommand.Parameters.AddWithValue("@Gia", cep.Gia)
+        objCommand.Parameters.AddWithValue("@CepId", cep.CepId)
+
+        objConexao.Open()
+        objCommand.ExecuteNonQuery()
+        objConexao.Close()
 
     End Sub
 
+    Public Sub DeletarCep(ByVal cepId As Integer)
 
+        strInstrucao = "DELETE FROM Cep WHERE CepId = @CepId"
+        objCommand.CommandText = strInstrucao
+        objCommand.Connection = objConexao
+        objCommand.Parameters.AddWithValue("@CepId", cepId)
 
-End Class
+    End Sub
 
-Public Class Chep
+    Public Function BuscarCep() As DataTable
 
-    Dim Cep As String = String.Empty
-    Dim Logradouro As String = String.Empty
-    Dim Complemento As String = String.Empty
-    Dim Bairro As String = String.Empty
-    Dim Localidade As String = String.Empty
-    Dim Uf As String = String.Empty
-    Dim Unidade As String = String.Empty
-    Dim Ibge As String = String.Empty
-    Dim Gia As String = String.Empty
+        Dim dt As New DataTable
+        Dim ds As New DataSet()
+        strInstrucao = "SELECT CepId, Logradouro, Complemento, Bairro, Localidade, Estado, Unidade, Ibge, Gia FROM Cep"
+        objCommand.CommandText = strInstrucao
+        objCommand.Connection = objConexao
+
+        objConexao.Open()
+        Dim da As New SqlDataAdapter(objCommand)
+        da.Fill(ds)
+        dt = ds.Tables(0)
+
+        objConexao.Close()
+        Return dt
+
+    End Function
 
 End Class
